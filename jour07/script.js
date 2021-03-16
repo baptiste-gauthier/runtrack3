a28bd240847a47a86cf6f788dd2d91469b3d8e44
baptiste-gauthier <baptiste.gauthier@laplateforme.io>
2021-03-16 16:50:50 +0100

    modal ok
    modal ok

---
+++ jour07/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#btn_papillon").click(function(){
+      $("#modal_papillon").modal("show");
+    });
+    // $("#modal_papillon").on('show.bs.modal', function () {
+ 
+    // });
+  });
